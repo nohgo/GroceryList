a3d87557e0b82dfd3b5ef6960ea678692156dcad
nohgo <60904645+nohgo@users.noreply.github.com>
2024-03-13 12:58:27 -0400

    Repo initialization

---
+++ src/main/java/com/github/nohgo/grocerylist/GroceryListApplication.java
@@ -0,0 +1,13 @@
+package com.github.nohgo.grocerylist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroceryListApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GroceryListApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/nohgo/grocerylist/GroceryListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.nohgo.grocerylist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroceryListApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
